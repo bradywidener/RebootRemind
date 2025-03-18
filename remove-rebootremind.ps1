5d767d759f260e3f106d8fa449cdf3af1326c401
brady.exe <bradywidener98@gmail.com>
2025-03-18 19:46:06 -0400

    Update remove-rebootremind.ps1

--- remove-rebootremind.ps1
+++ remove-rebootremind.ps1
@@ -1,5 +1,5 @@
 #Removing Scheduled Task
-Unregister-ScheduledTask -TaskName "MyPowerShellTask" -Confirm:$false
+Unregister-ScheduledTask -TaskName "RebootRemindCheck" -Confirm:$false
 
 #Removing the program from program files
 Remove-Item -Path "C:\Program Files\rebootremind" -Recurse -Force
